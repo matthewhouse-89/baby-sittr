37dbad8c2cba917839f5f4689b527ea78318063d
Matthew House <matthew.house89@gmail.com>
2021-05-10 19:37:11 -0400

    end time of 4 or earlier is valid

--- lib/baby_sittr.rb
+++ lib/baby_sittr.rb
@@ -6,6 +6,6 @@ class BabySittr
 
     def valid?
         valid_period = ['5PM','6PM', '7PM', '8PM', '9PM', '10PM', '11PM', '12AM', '1AM', '2AM', '3AM', '4AM']
-        valid_period.include?(@start_time)
+        valid_period.include?(@start_time) && valid_period.include?(@end_time)
     end
 end
--- spec/baby_sittr_spec.rb
+++ spec/baby_sittr_spec.rb
@@ -23,8 +23,30 @@ RSpec.describe BabySittr do
                 expect(result).to be_falsey
             end
 
-            it 'returns true if start time is 3PM' do
+            it 'returns false if start time is 3PM' do
                 result = BabySittr.new(start_time: '3PM', end_time: '7PM').valid?
+                expect(result).to be_falsey
+            end
+        end
+
+        context 'ends no later than 4:00PM' do
+            it 'returns true if end time is 4AM' do
+                result = BabySittr.new(start_time: '5PM', end_time: '4AM').valid?
+                expect(result).to be_truthy
+            end
+
+            it 'returns true if end time is 3AM' do
+                result = BabySittr.new(start_time: '5PM', end_time: '3AM').valid?
+                expect(result).to be_truthy
+            end
+
+            it 'returns false if end time is 6AM' do
+                result = BabySittr.new(start_time: '5PM', end_time: '6AM').valid?
+                expect(result).to be_falsey
+            end
+
+            it 'returns false if end time is 7AM' do
+                result = BabySittr.new(start_time: '5PM', end_time: '7AM').valid?
                 expect(result).to be_falsey
             end
         end
